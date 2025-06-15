c6dd5e694ee975902392ea9cfbc867bfc50dd2b0
kata-list <katalist47@gmail.com>
2025-06-15 15:58:48 +0600

    init island properties

--- entypoints.mjs
+++ entypoints.mjs
@@ -2,4 +2,8 @@
  * This file defines the entry points for the application.
  * Example: ['src/target_folder/target_file.ts', 'src/target_folder2/target_file2.ts']
  */
-export const ENTRY_POINTS = ["src/index.ts"];
+export const ENTRY_POINTS = [
+  "src/intl-tel-input.ts",
+  "src/multi-step-form.ts",
+  "src/search-address.ts",
+];
